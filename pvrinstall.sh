9783bd71c62c4b4213cb209922cc36290eaed213
Gabriel Zellmer <gabe@crosseye.org>
2013-04-23 14:53:51 -0500

    Switched SABnzbd from GitHub to SABnzbd+

--- pvrinstall.sh
+++ pvrinstall.sh
@@ -11,7 +11,7 @@ sleep 2
 disclaimer ()
 {
 clear
-echo "PVRinstall script will install SABnzbd, SickBeard, CouchPotato and headphones."
+echo "PVRinstall script will install SABnzbd+, SickBeard, CouchPotato and Headphones."
 echo "Copyright (C) 2013  CrossEye"
 echo ""
 echo "This program is free software: you can redistribute it and/or modify"
@@ -38,43 +38,43 @@ do disclaimer
 			clear
 			echo "Updating APT package indexes."
 			sleep 2
-			apt-get update -q=3
+			apt-get update -q=2
 			echo "Installing dependencies."
 			sleep 2
-			apt-get install -y -q=3 openssh-server
-			apt-get install -y -q=3 ntp
-			apt-get install -y -q=3 apache2
-			apt-get install -y -q=3 git
-			apt-get install -y -q=3 git-core
-			apt-get install -y -q=3 python
-			apt-get install -y -q=3 python-cheetah
-			apt-get install -y -q=3 python-configobj
-			apt-get install -y -q=3 python-feedparser
-			apt-get install -y -q=3 python-dbus
-			apt-get install -y -q=3 python-openssl
-			apt-get install -y -q=3 python-support
-			apt-get install -y -q=3 python-yenc
-			apt-get install -y -q=3 par2
-			apt-get install -y -q=3 unrar
-			apt-get install -y -q=3 unzip
+			apt-get install -y -q=2 openssh-server
+			apt-get install -y -q=2 ntp
+			apt-get install -y -q=2 apache2
+			apt-get install -y -q=2 git
+			apt-get install -y -q=2 git-core
+			apt-get install -y -q=2 python
+			apt-get install -y -q=2 python-cheetah
+			apt-get install -y -q=2 python-configobj
+			apt-get install -y -q=2 python-feedparser
+			apt-get install -y -q=2 python-dbus
+			apt-get install -y -q=2 python-openssl
+			apt-get install -y -q=2 python-support
+			apt-get install -y -q=2 python-yenc
+			apt-get install -y -q=2 python-notify
+			apt-get install -y -q=2 par2
+			apt-get install -y -q=2 unrar
+			apt-get install -y -q=2 unzip
+			apt-get install -y -q=2 sabnzbdplus
+			apt-get install -y -q=2 sabnzbdplus-theme-mobile
 			echo "Creating install locations"
 			sleep 2
 			mkdir -p /opt/pvrinitscripts
-			mkdir -p /opt/sabnzbd
 			mkdir -p /opt/sickbeard
 			mkdir -p /opt/couchpotato
 			mkdir -p /opt/headphones
 			echo "Cloning necessary GIT repositories."
 			sleep 2
 			git clone git://github.com/CrossEyeORG/PVRinitScripts.git /opt/pvrinitscripts
-			git clone git://github.com/sabnzbd/sabnzbd.git /opt/sabnzbd
 			git clone git://github.com/midgetspy/Sick-Beard.git /opt/sickbeard
 			git clone git://github.com/RuudBurger/CouchPotatoServer.git /opt/couchpotato
 			git clone git://github.com/rembo10/headphones.git /opt/headphones
 			echo "Setting permissions."
 			sleep 2
 			chmod 777 -R /opt/pvrinitscripts
-			chmod 777 -R /opt/sabnzbd
 			chmod 777 -R /opt/sickbeard
 			chmod 777 -R /opt/couchpotato
 			chmod 777 -R /opt/headphones
@@ -120,24 +120,22 @@ do initconfig
 	read USRNAME
 	echo "Configuring specified username."
 	sleep 2
+	sed -i -e "s/USER=/USER=$USRNAME/g" /etc/default/sabnzbdplus
 	sed -i -e "s/CHANGEME/$USRNAME/g" /opt/pvrinitscripts/SickBeard.sh
 	sed -i -e "s/CHANGEME/$USRNAME/g" /opt/pvrinitscripts/CouchPotato.sh
 	sed -i -e "s/CHANGEME/$USRNAME/g" /opt/pvrinitscripts/Headphones.sh
 	echo "Copying configured init scripts to /etc/init.d/."
 	sleep 2
-	cp /opt/pvrinitscripts/SABnzbd.sh /etc/init.d/sabnzbd
 	cp /opt/pvrinitscripts/SickBeard.sh /etc/init.d/sickbeard
 	cp /opt/pvrinitscripts/CouchPotato.sh /etc/init.d/couchpotato
 	cp /opt/pvrinitscripts/Headphones.sh /etc/init.d/headphones
 	echo "Making the new init scripts executable."
 	sleep 2
-	chmod +x /etc/init.d/sabnzbd
 	chmod +x /etc/init.d/sickbeard
 	chmod +x /etc/init.d/couchpotato
 	chmod +x /etc/init.d/headphones
 	echo "Updating the systems services list."
 	sleep 2
-	update-rc.d sabnzbd defaults
 	update-rc.d sickbeard defaults
 	update-rc.d couchpotato defaults
 	update-rc.d headphones defaults
@@ -156,7 +154,7 @@ do startservices
 	case "$CHOICE" in
 		"y")
 			clear
-			service sabnzbd start
+			service sabnzbdplus start
 			service sickbeard start
 			service couchpotato start
 			service headphones start
